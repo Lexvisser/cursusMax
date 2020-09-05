b6cba1aa4d53cd141afa81fcd253251cf35103c9
Lexvisser <lexvisser@gmail.com>
2020-09-05 18:10:15 +0200

    Max29

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('Hello!'),
+      home: Scaffold(
+        appBar: AppBar(
+            title: Text('My first Barretej'), backgroundColor: Colors.blueGrey),
+        body: Text('wat maak je me nuu'),
+      ),
     );
   }
 }
