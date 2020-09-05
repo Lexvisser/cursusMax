22eec6d769f7ee18e782f3e2dad80f1f658cb6a5
Lexvisser <lexvisser@gmail.com>
2020-09-05 20:09:35 +0200

    Max32

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,10 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('answer chosen');
+  }
+
   @override
   Widget build(BuildContext context) {
     var questions = ['what\'s your favorite color', 'what\'s your penislength'];
@@ -15,14 +19,14 @@ class MyApp extends StatelessWidget {
             Text('the question'),
             RaisedButton(
               child: Text('Answer 1'),
-              onPressed: null,
+              onPressed: answerQuestion,
             ),
             RaisedButton(
               child: Text('Answer 2'),
-              onPressed: null,
+              onPressed: answerQuestion,
               color: Colors.green[400],
             ),
-            RaisedButton(child: Text('Answer 3'), onPressed: null),
+            RaisedButton(child: Text('Answer 3'), onPressed: answerQuestion),
           ],
         ),
       ),
