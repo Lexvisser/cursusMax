947a313e192a978a9549de1d27640bc3426e8917
Lexvisser <lexvisser@gmail.com>
2020-09-05 19:30:53 +0200

    Max31

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,25 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var questions = ['what\'s your favorite color', 'what\'s your penislength'];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-            title: Text('My first Barretej'), backgroundColor: Colors.blueGrey),
-        body: Text('wat maak je me nu'),
+            title: Text('My first Barretje'), backgroundColor: Colors.blue),
+        body: Column(
+          children: [
+            Text('the question'),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: null,
+            ),
+            RaisedButton(child: Text('Answer 3'), onPressed: null),
+          ],
+        ),
       ),
     );
   }
