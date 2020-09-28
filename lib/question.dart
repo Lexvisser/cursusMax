b8d47968f019e3a1420bb09cf4a175890bf2378c
Lexvisser <lexvisser@gmail.com>
2020-09-28 18:34:54 +0200

    Max40

--- lib/question.dart
+++ lib/question.dart
@@ -5,8 +5,28 @@ class Question extends StatelessWidget {
 
   Question(this.questionText);
 
+  BoxDecoration myBoxDecoration() {
+    return BoxDecoration(
+        color: Colors.orange,
+        border: Border.all(
+          color: Colors.red, //                   <--- border color
+          width: 5.0,
+        ),
+        borderRadius: BorderRadius.all(Radius.circular(30.0)));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text(questionText);
+    return Container(
+        width: double.infinity,
+        margin: EdgeInsets.all(20),
+        padding: EdgeInsets.all(10),
+        decoration: myBoxDecoration(),
+        child: Text(
+          questionText,
+          style: TextStyle(
+              fontSize: 28, fontWeight: FontWeight.bold, color: Colors.blue),
+          textAlign: TextAlign.center,
+        ));
   }
 }
